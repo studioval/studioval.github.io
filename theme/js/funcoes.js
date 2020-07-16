56bbe74b3a51f1ec758e32a0bcc026996b3e7b05
studioval <67485604+studioval@users.noreply.github.com>
2020-07-16 18:57:26 -0300

    Add files via upload

--- theme/js/funcoes.js
+++ theme/js/funcoes.js
@@ -1,5 +1,4 @@
 /*global $ */
-
 var loc, pathName, alt;
 
 $(document).ready(function () {
@@ -10,32 +9,12 @@ $(document).ready(function () {
         $('html').toggleClass('menu-active');
         e.preventDefault();
     });
-//    Altura do site
-    alt = $(window).height();
-//    Altura do site menos topo
-    alt = alt - $('.header').height();
-//    Altura do site menos banner e rodape
-    alt = alt - $('.banner-principal').height();
-    alt = alt - $('.footer').height();
-    $('.interna').css({"min-height": +alt + "px"});
     
 });
 var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   direction: 'vertical',
   loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
